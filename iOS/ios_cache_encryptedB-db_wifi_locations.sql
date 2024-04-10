e040ffa82f3cd354abe71c22f6533a69ecb43782
LongRangeBehaviorModificationSpecialist <mikespon@gmail.com>
2024-04-10 05:57:29 -0400

    update file

--- iOS/ios_cache_encryptedB-db_wifi_locations.sql
+++ iOS/ios_cache_encryptedB-db_wifi_locations.sql
@@ -1,15 +1,19 @@
 SELECT
-    MAC AS 'MAC (as dec)',
-    printf("%x", MAC) AS 'MAC (as hex)',
-    CASE
-        WHEN WifiLocation.MAC IS NULL THEN 'n/a'
-        WHEN LENGTH(MAC) > 12 THEN CONCAT('0', MAC)
-        ELSE CONCAT('', MAC)
-    END AS 'Test',
-
-    typeof(MAC),
+    ROW_NUMBER() OVER() AS 'Record No.',
+    UPPER(substr(printf("%0!.12x", MAC), 1, 2) || ':' ||
+        substr(printf("%0!.12x", MAC), 3, 2) || ':' ||
+        substr(printf("%0!.12x", MAC), 5, 2) || ':' ||
+        substr(printf("%0!.12x", MAC), 7, 2) || ':' ||
+        substr(printf("%0!.12x", MAC), 9, 2) || ':' ||
+        substr(printf("%0!.12x", MAC), 11, 2)) AS 'MAC Address',
+    Channel AS 'Channel',
     strftime('%Y-%m-%dT%H:%M:%SZ', datetime(timestamp + 978307200, 'UNIXEPOCH')) AS 'Timestamp (UTC)',
-    Channel
+    strftime('%Y-%m-%d %H:%M:%S', datetime(timestamp + 978307200, 'UNIXEPOCH', 'localtime')) AS 'Timestamp (Local)',
+    Latitude AS 'Latitude',
+    Longitude AS 'Longitude',
+    HorizontalAccuracy AS 'HorizontalAccuracy',
+    Altitude AS 'Altitude',
+    Confidence AS 'Confidence'
 
 FROM WifiLocation
 
