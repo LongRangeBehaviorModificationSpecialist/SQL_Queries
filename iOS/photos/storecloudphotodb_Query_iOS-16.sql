8545cc303eda5ea4b9df9c3ab18ceaa27ae9f7ba
LongRangeBehaviorModificationSpecialist <mikespon@gmail.com>
2025-07-18 06:44:55 -0400

    reformatted files

--- iOS/photos/storecloudphotodb_Query_iOS-16.sql
+++ iOS/photos/storecloudphotodb_Query_iOS-16.sql
@@ -3,126 +3,154 @@ Query copied from https://github.com/ScottKjr3347/iOS_store.cloudphotodb_Queries
 */
 
 SELECT
-	ROW_NUMBER() OVER() AS 'Record #',
-	datetime(scopes.creationDate + 978307200, 'UNIXEPOCH') AS 'scopes-creationDate',
-	scopes.scopeIdentifier AS 'scopes-scopeIdentifier = Photos.sqlite_zShare-ScopeID',
-	datetime(scopes.initialSyncDate + 978307200, 'UNIXEPOCH') AS 'scopes- initialSyncDate',
-	cloudCache.class AS 'cloudCache-class',
-	statusCenter.class AS 'statusCenter- class',
-	cloudCache.serializedRecord AS 'cloudCache- serializedRecord',
-	CASE idMapping.addDirection
-		WHEN 1 THEN 'Device2iCloudPhotos-1'
-		WHEN 2 THEN 'iCloudPhotos2Devcie-2'
-		ELSE 'Unknown-New-Value!: ' || idMapping.addDirection || ''
-	END AS 'idMapping-addDirection',
-	CASE downloadQueue.resourceType
-		WHEN 5 THEN 'iCloud Photos-5'
-		ELSE 'Unknown-New-Value!: ' || downloadQueue.resourceType || ''
-	END AS 'downloadQueue-resourceType',
-	CASE downloadQueue.status
-		WHEN 2 THEN 'Synced with Device-2'
-		ELSE 'Unknown-New-Value!: ' || downloadQueue.status || ''
-	END AS 'downloadQueue-status',
-	CASE idMapping.mappingState
-		WHEN 1 THEN 'Successful-1'
-		WHEN 2 THEN 'StillTesting-2'
-		ELSE 'Unknown-New-Value!: ' || idMapping.mappingState || ''
-	END AS 'idMapping-mappingState',
-	CASE downloadQueue.intent
-		WHEN 1 THEN 'Completed-1'
-		ELSE 'Unknown-New-Value!: ' || downloadQueue.intent || ''
-	END AS 'downloadQueue-intent',
-	downloadQueue.position AS 'downloadQueue-position',
-	downloadQueue.taskIdentifier AS 'downloadQueue-taskIdentifier',
-	downloadQueue.retryCount AS 'downloadQueue-retryCount',
-	datetime(idMapping.addTimestamp + 978307200, 'UNIXEPOCH') AS 'idMapping-addTimestamp',
-	CASE cloudCache.confirmed
-		WHEN 1 THEN 'Yes-1'
-		WHEN 2 THEN 'StillTesting-2'
-		ELSE 'Unknown-New-Value!: ' || cloudCache.confirmed || ''
-	END AS 'cloudCache-confirmed',
-	CASE cloudCache.state
-		WHEN 1 THEN 'Active-1'
-		ELSE 'Unknown-New-Value!: ' || cloudCache.state || ''
-	END AS 'cloudCache-state',
-	datetime(scopes.disabledDate + 978307200, 'UNIXEPOCH') AS 'scopes-disabledDate',
-	datetime(scopes.deleteDate + 978307200, 'UNIXEPOCH') AS 'scopes-deleteDate = zShare-ExpiryDate',
-	datetime(idMapping.deleteTimestamp + 978307200, 'UNIXEPOCH') AS 'idMapping-deleteTimestamp',
-	CASE idMapping.deleteDirection
-		WHEN 1 THEN 'Deleted via Recently Deleted via Device Photos App-1'
-		WHEN 2 THEN 'StillTesting-2'
-		ELSE 'Unknown-New-Value!: ' || idMapping.deleteDirection || ''
-	END AS 'idMapping-deleteDirection',
-	cloudCache.identifier AS 'cloudCache- identifier = Photos.sqlite-AssetUUID-CldMstGUID',
-	idMapping.cloudIdentifier AS 'idMapping- cloudIdentifier = Photos.sqlite-AssetUUID-CldMstGUID',
-	remappedDeletes.cloudIdentifier AS 'remappedDeletes- Cloud Identifier',
-	idMapping.localIdentifier AS 'idMapping- localIdentifier = Photos.sqlite-AssetUUID-CldMstGUID',
-	statusCenter.identifier AS 'statusCenter- identifier',
-	downloadQueue.itemIdentifier AS 'downloadQueue-itemIdentifier = Photos.sqlite-AssetUUID-CldMastGUID',
-	downloadQueue.fingerPrint AS 'downloadQueue-fingerPrint = Photos.sqlite_zAsset-zIntResou-Fingerprint',
-	cloudCache.relatedIdentifier AS 'cloudCache- relatedIdentifier = Photos.sqlite-AssetUUID-CldMstGUID',
-	remappedDeletes.realIdentifier AS 'remappedDeletes- Real Identifier',
-	cloudCache.secondaryIdentifier AS 'cloudCache- secondaryIdentifier = Photos.sqlite-GenAlbGUID',
-	cloudCache.stagedSerializedRecord AS 'cloudCache- stagedSerializedRecord',
-	cloudCache.stagedRelatedIdentifier AS 'cloudCache- stagedRelatedIdentifier',
-	cloudCache.stagedSecondaryIdentifier AS 'cloudCache- stagedSecondaryIdentifier',
-	downloadQueue.fileUTI AS 'downloadQueue-fileUTI',
-	downloadQueue.fileSize AS 'downloadQueue-fileSize',
-	scopes.estimatedSize AS 'scopes- estimatedSize',
-	scopes.estimatedAssetCount AS 'scopes- estimatedAssetCount',
-	datetime(scopes.lastClearedPush + 978307200, 'UNIXEPOCH') AS 'scopes- lastClearedPush',
-	datetime(scopes.lastLibraryInfoUpdate + 978307200, 'UNIXEPOCH') AS 'scopes- last Library InfoUpdate',
-	datetime(scopes.pullFromTransportLastCompletionDate + 978307200, 'UNIXEPOCH') AS 'scopes- pull From Transport Last CompletionDate',
-	scopes.droppedSomeRecords AS 'scopes- dropped Some Records',
-	scopes.featureVersion AS 'scopes- feature Version',
-	scopes.flags AS 'scopes-flags',
-	CASE scopes.hasFetchedInitialSyncAnchor
-		WHEN 1 THEN 'Yes-1'
-		ELSE 'Unknown-New-Value!: ' || scopes.hasFetchedInitialSyncAnchor || ''
-	END AS 'scopes- has Fetched initial Sync Anchor',
-	scopes.initialSyncAnchor AS 'scopes-initial Sync Anchor',
-	scopes.syncAnchor AS 'scopes- syncAnchor',
-	scopes.stagedSyncAnchor AS 'scopes- staged Sync Anchor',
-	scopes.transientSyncAnchor AS 'scopes- transient SyncAnchor',
-	scopes.libraryInfo AS 'scopes- libraryInfo',
-	scopes.scopeChange AS 'scopes- scopeChange',
-	scopes.transportScope AS 'scopes- transportScope',
-	scopes.classForInitialQuery AS 'scopes- class For Initial Query',
-	scopes.uploadTransportGroup AS 'scopes- upload Transport Group',
-	scopes.downloadTransportGroup AS 'scopes- download Transport Group',
-	scopes.pushToTransportCurrentGeneration AS 'scopes- push To Transport Current Generation',
-	scopes.pushToTransportTargetGeneration AS 'scopes- push To Transport Target Generation',
-	scopes.pullFromTransportTargetGeneration AS 'scopes- pull From Transport Target Generation',
-	scopes.pullFromTransportExpirationInterval AS 'scopes- pull From Transport Expiration Interval',
-	scopes.clientNeedsToPullCurrentGeneration AS 'scopes- client Needs To Pull Current Generation',
-	scopes.clientNeedsToPullTargetGeneration AS 'scopes- client Needs To Pull Target Generation',
-	scopes.updateTransportCurrentGeneration AS 'scopes- update Transport Current Generation',
-	scopes.updateTransportTargetGeneration AS 'scopes- update Transport Target Generation',
-	pushRepository.class AS 'pushRepository- class',
-	pushRepository.scopeIndex AS 'pushRepository- scopeIndex',
-	pushRepository.identifier AS 'pushRepository- identifier',
-	pushRepository.changeType AS 'pushRepository- changeType',
-	pushRepository.relatedIdentifier AS 'pushRepository- relatedIdentifier',
-	pushRepository.secondaryIdentifier AS 'pushRepository- secondaryIdentifier',
-	datetime(pushRepository.dequeueOrder + 978307200, 'UNIXEPOCH') AS 'pushRepository- dequeueOrder',
-	pushRepository.trashed AS 'pushRepository- trashed',
-	pushRepository.uploadIdentifier AS 'pushRepository- upload Identifier',
-	pushRepository.trustLevel AS 'pushRepository- trust Level',
-	pushRepository.serializedRecord AS 'pushRepository- serialized Record',
-	statusCenter.generation AS 'statusCenter- generation'
+    ROW_NUMBER() OVER() AS 'Record #',
+
+    datetime(scopes.creationDate + 978307200, 'UNIXEPOCH') AS 'scopes.creationDate',
+
+    -- [scopes.scopeIdentifier = Photos.sqlite_zShare.ScopeID]
+    scopes.scopeIdentifier AS 'scopes.scopeIdentifier',
+
+    datetime(scopes.initialSyncDate + 978307200, 'UNIXEPOCH') AS 'scopes.initialSyncDate',
+
+    cloudCache.class AS 'cloudCache.class',
+    statusCenter.class AS 'statusCenter.class',
+    cloudCache.serializedRecord AS 'cloudCache.serializedRecord',
+
+    CASE idMapping.addDirection
+        WHEN 1 THEN '1  [Device2iCloudPhotos]'
+        WHEN 2 THEN '2  [iCloudPhotos2Devcie]'
+        ELSE 'Unknown Value: ' || idMapping.addDirection || ''
+    END AS 'idMapping.addDirection',
+
+    CASE downloadQueue.resourceType
+        WHEN 5 THEN '5  [iCloud Photos]'
+        ELSE 'Unknown Value: ' || downloadQueue.resourceType || ''
+    END AS 'downloadQueue.resourceType',
+
+    CASE downloadQueue.status
+        WHEN 2 THEN '2  [Synced with Device]'
+        ELSE 'Unknown Value: ' || downloadQueue.status || ''
+    END AS 'downloadQueue.status',
+
+    CASE idMapping.mappingState
+        WHEN 1 THEN '1  [Successful]'
+        WHEN 2 THEN '2  [Still Testing]'
+        ELSE 'Unknown Value: ' || idMapping.mappingState || ''
+    END AS 'idMapping.mappingState',
+
+    CASE downloadQueue.intent
+        WHEN 1 THEN '1  [Completed]'
+        ELSE 'Unknown Value: ' || downloadQueue.intent || ''
+    END AS 'downloadQueue.intent',
+
+    downloadQueue.position AS 'downloadQueue.position',
+    downloadQueue.taskIdentifier AS 'downloadQueue.taskIdentifier',
+    downloadQueue.retryCount AS 'downloadQueue.retryCount',
+
+    datetime(idMapping.addTimestamp + 978307200, 'UNIXEPOCH') AS 'idMapping.addTimestamp(UTC)',
+
+    CASE cloudCache.confirmed
+        WHEN 1 THEN '1  [Yes]'
+        WHEN 2 THEN '2  [Still Testing]'
+        ELSE 'Unknown Value: ' || cloudCache.confirmed || ''
+    END AS 'cloudCache.confirmed',
+
+    CASE cloudCache.state
+        WHEN 1 THEN 'Active-1'
+        ELSE 'Unknown Value: ' || cloudCache.state || ''
+    END AS 'cloudCache.state',
+
+    datetime(scopes.disabledDate + 978307200, 'UNIXEPOCH') AS 'scopes.disabledDate(UTC)',
+    -- [scopes.deleteDate = zShare.ExpiryDate]
+    datetime(scopes.deleteDate + 978307200, 'UNIXEPOCH') AS 'scopes.deleteDate(UTC)',
+    datetime(idMapping.deleteTimestamp + 978307200, 'UNIXEPOCH') AS 'idMapping.deleteTimestamp(UTC)',
+
+    CASE idMapping.deleteDirection
+        WHEN 1 THEN '1  [Deleted via Recently Deleted via Device Photos App]'
+        WHEN 2 THEN '2  [Still Testing]'
+        ELSE 'Unknown Value: ' || idMapping.deleteDirection || ''
+    END AS 'idMapping.deleteDirection',
+
+    -- [cloudCache.identifier = Photos.sqlite_AssetUUID.CldMstGUID]
+    cloudCache.identifier AS 'cloudCache.identifier',
+    -- [idMapping.cloudIdentifier = Photos.sqlite_AssetUUID.CldMstGUID]
+    idMapping.cloudIdentifier AS 'idMapping.cloudIdentifier',
+    remappedDeletes.cloudIdentifier AS 'remappedDeletes.Cloud Identifier',
+    -- [idMapping.localIdentifier = Photos.sqlite_AssetUUID.CldMstGUID]
+    idMapping.localIdentifier AS 'idMapping.localIdentifier',
+    statusCenter.identifier AS 'statusCenter.identifier',
+    -- [downloadQueue.itemIdentifier = Photos.sqlite_AssetUUID.CldMastGUID]
+    downloadQueue.itemIdentifier AS 'downloadQueue.itemIdentifier',
+    -- [downloadQueue.fingerPrint = Photos.sqlite_zAsset-zIntResouFingerprint]
+    downloadQueue.fingerPrint AS 'downloadQueue.fingerPrint',
+    -- [cloudCache.relatedIdentifier = Photos.sqlite_AssetUUID.CldMstGUID]
+    cloudCache.relatedIdentifier AS 'cloudCache.relatedIdentifier',
+    remappedDeletes.realIdentifier AS 'remappedDeletes.Real Identifier',
+    -- [cloudCache.secondaryIdentifier = Photos.sqlite_GenAlb.GUID]
+    cloudCache.secondaryIdentifier AS 'cloudCache.secondaryIdentifier',
+    cloudCache.stagedSerializedRecord AS 'cloudCache.stagedSerializedRecord',
+    cloudCache.stagedRelatedIdentifier AS 'cloudCache.stagedRelatedIdentifier',
+    cloudCache.stagedSecondaryIdentifier AS 'cloudCache.stagedSecondaryIdentifier',
+    downloadQueue.fileUTI AS 'downloadQueue.fileUTI',
+    downloadQueue.fileSize AS 'downloadQueue.fileSize',
+    scopes.estimatedSize AS 'scopes.estimatedSize',
+    scopes.estimatedAssetCount AS 'scopes.estimatedAssetCount',
+
+    datetime(scopes.lastClearedPush + 978307200, 'UNIXEPOCH') AS 'scopes.lastClearedPush(UTC)',
+    datetime(scopes.lastLibraryInfoUpdate + 978307200, 'UNIXEPOCH') AS 'scopes.lastLibraryInfoUpdate(UTC)',
+    datetime(scopes.pullFromTransportLastCompletionDate + 978307200, 'UNIXEPOCH') AS 'scopes.pullFromTransportLastCompletionDate(UTC)',
+
+    scopes.droppedSomeRecords AS 'scopes.droppedSomeRecords',
+    scopes.featureVersion AS 'scopes.featureVersion',
+    scopes.flags AS 'scopes.flags',
+
+    CASE scopes.hasFetchedInitialSyncAnchor
+        WHEN 1 THEN '1  [Yes]'
+        ELSE 'Unknown Value: ' || scopes.hasFetchedInitialSyncAnchor || ''
+    END AS 'scopes.hasFetchedInitialSyncAnchor',
+
+    scopes.initialSyncAnchor AS 'scopes.initialSyncAnchor',
+    scopes.syncAnchor AS 'scopes.syncAnchor',
+    scopes.stagedSyncAnchor AS 'scopes.stagedSyncAnchor',
+    scopes.transientSyncAnchor AS 'scopes.transientSyncAnchor',
+    scopes.libraryInfo AS 'scopes.libraryInfo',
+    scopes.scopeChange AS 'scopes.scopeChange',
+    scopes.transportScope AS 'scopes.transportScope',
+    scopes.classForInitialQuery AS 'scopes.classForInitialQuery',
+    scopes.uploadTransportGroup AS 'scopes.uploadTransportGroup',
+    scopes.downloadTransportGroup AS 'scopes.downloadTransportGroup',
+    scopes.pushToTransportCurrentGeneration AS 'scopes.pushToTransportCurrentGeneration',
+    scopes.pushToTransportTargetGeneration AS 'scopes.pushToTransportTargetGeneration',
+    scopes.pullFromTransportTargetGeneration AS 'scopes.pullFromTransportTargetGeneration',
+    scopes.pullFromTransportExpirationInterval AS 'scopes.pullFromTransportExpirationInterval',
+    scopes.clientNeedsToPullCurrentGeneration AS 'scopes.clientNeedsToPullCurrentGeneration',
+    scopes.clientNeedsToPullTargetGeneration AS 'scopes.clientNeedsToPullTargetGeneration',
+    scopes.updateTransportCurrentGeneration AS 'scopes.updateTransportCurrentGeneration',
+    scopes.updateTransportTargetGeneration AS 'scopes.updateTransportTargetGeneration',
+    pushRepository.class AS 'pushRepository.class',
+    pushRepository.scopeIndex AS 'pushRepository.scopeIndex',
+    pushRepository.identifier AS 'pushRepository.identifier',
+    pushRepository.changeType AS 'pushRepository.changeType',
+    pushRepository.relatedIdentifier AS 'pushRepository.relatedIdentifier',
+    pushRepository.secondaryIdentifier AS 'pushRepository.secondaryIdentifier',
+
+    datetime(pushRepository.dequeueOrder + 978307200, 'UNIXEPOCH') AS 'pushRepository.dequeueOrder(UTC)',
+
+    pushRepository.trashed AS 'pushRepository.trashed',
+    pushRepository.uploadIdentifier AS 'pushRepository.uploadIdentifier',
+    pushRepository.trustLevel AS 'pushRepository.trustLevel',
+    pushRepository.serializedRecord AS 'pushRepository.serializedRecord',
+    statusCenter.generation AS 'statusCenter.generation'
+
 
 FROM idMapping
-	LEFT JOIN
-		scopes ON scopes.stableIndex = idMapping.stableScopeIndex
-	LEFT JOIN
-		cloudCache ON cloudCache.identifier = idMapping.cloudIdentifier
-	LEFT JOIN
-		pushRepository ON pushRepository.identifier = cloudCache.identifier
-	LEFT JOIN
-		downloadQueue ON downloadQueue.itemIdentifier = cloudCache.identifier
-	LEFT JOIN
-		statusCenter ON statusCenter.identifier = idMapping.cloudIdentifier
-	LEFT JOIN
-		remappedDeletes ON remappedDeletes.cloudIdentifier = idMapping.cloudIdentifier
+
+    LEFT JOIN scopes ON scopes.stableIndex = idMapping.stableScopeIndex
+    LEFT JOIN cloudCache ON cloudCache.identifier = idMapping.cloudIdentifier
+    LEFT JOIN pushRepository ON pushRepository.identifier = cloudCache.identifier
+    LEFT JOIN downloadQueue ON downloadQueue.itemIdentifier = cloudCache.identifier
+    LEFT JOIN statusCenter ON statusCenter.identifier = idMapping.cloudIdentifier
+    LEFT JOIN remappedDeletes ON remappedDeletes.cloudIdentifier = idMapping.cloudIdentifier
+
 
 ORDER BY idMapping.addTimestamp
