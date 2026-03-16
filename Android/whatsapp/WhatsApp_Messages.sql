12f1878fe5ca62fced87fc179e810f9ee8a3cb7a
LongRangeBehaviorModificationSpecialist <mikespon@gmail.com>
2026-03-16 10:38:41 -0400

    edited query

--- Android/whatsapp/WhatsApp_Messages.sql
+++ Android/whatsapp/WhatsApp_Messages.sql
@@ -2,36 +2,55 @@
 Query copied from https://thebinaryhick.blog/2022/06/09/new-msgstore-who-dis-a-look-at-an-updated-whatsapp-on-android/
 */
 
-ATTACH DATABASE "wa.db" AS 'whatsapp_db';
+ATTACH DATABASE 'file:F:\26-03686_ADAMSON\EVIDENCE\EXPORTS\com.whatsapp\msgstore[dot]db\wa.db?mode=ro' AS whatsapp_db;
 
 
 SELECT
-    ROW_NUMBER() OVER() AS 'RECORD_NUMBER',
-    message._id AS 'message._id',
+    ROW_NUMBER() OVER(ORDER BY message._id) AS 'RecordNo.',
+    message._id AS 'MessageID',
 
-    datetime(message.timestamp / 1000, 'UNIXEPOCH') AS 'message.timestamp(UTC)',
+    CASE message.from_me
+        WHEN 0 THEN '0 [Incoming]'
+        WHEN 1 THEN '1 [Outgoing]'
+    END AS 'MessageFromMe',
+
+    datetime(message.timestamp / 1000, 'UNIXEPOCH') AS 'MessageTimestamp(UTC)',
 
     CASE
-        WHEN datetime(message.received_timestamp / 1000, 'UNIXEPOCH') = '1970-01-01 00:00:00' THEN '[N/A]'
+        WHEN datetime(message.received_timestamp / 1000, 'UNIXEPOCH') = '1970-01-01 00:00:00' THEN NULL
         ELSE datetime(message.received_timestamp / 1000, 'UNIXEPOCH')
     END AS 'message.received_timestamp(UTC)',
 
-    CASE message.from_me
-        WHEN 0 THEN '0  [Incoming]'
-        WHEN 1 THEN '1  [Outgoing]'
-    END AS 'message.from_me',
-
-    CASE message.recipient_count
-        WHEN 0 THEN '0  [No]'
-        WHEN 1 THEN '1  [Yes]'
-    END AS 'message.recipient_count',
+    CASE
+        WHEN datetime(message.receipt_server_timestamp / 1000, 'UNIXEPOCH') = '1970-01-01 00:00:00' THEN NULL
+        ELSE datetime(message.receipt_server_timestamp / 1000, 'UNIXEPOCH')
+    END AS 'ReceiptServerTimestamp(UTC)',
 
     CASE
-        WHEN wa_contacts.wa_name IS NULL THEN '[N/A]'
-        ELSE wa_contacts.wa_name
-    END AS 'wa_contacts.wa_name',
+        WHEN datetime(receipt_device.receipt_device_timestamp / 1000, 'UNIXEPOCH') = '1970-01-01 00:00:00' THEN NULL
+        ELSE datetime(receipt_device.receipt_device_timestamp / 1000, 'UNIXEPOCH')
+    END AS 'ReceiptDeviceTimestamp(UTC)',
 
-    wa_contacts.display_name AS 'wa_contacts.display_name',
+    CASE
+        WHEN datetime(receipt_user.read_timestamp / 1000, 'UNIXEPOCH') = '1970-01-01 00:00:00' THEN NULL
+        ELSE datetime(receipt_user.read_timestamp / 1000, 'UNIXEPOCH')
+    END AS 'ReadTimestamp(UTC)',
+
+    CASE message.recipient_count
+        WHEN 0 THEN '0 [No]'
+        WHEN 1 THEN '1 [Yes]'
+    END AS 'MessageRecipientCount',
+
+    CASE
+        WHEN wa_contacts.wa_name IS NULL THEN 'NotListed'
+        ELSE wa_contacts.wa_name
+    END AS 'OtherParticipantWAUserName',
+
+    jid.raw_string AS 'JidRawString',
+
+    wa_contacts.number AS 'ContactNumber',
+
+    wa_contacts.display_name AS 'WADisplayUserName',
 
     CASE message.message_type
         WHEN 0 THEN '0  [Text]'
@@ -46,67 +65,95 @@ SELECT
         WHEN 15 THEN '15  [Sticker]'
         WHEN 16 THEN '16  [Live Location]'
         ELSE message.message_type
-    END AS 'message.message_type',
+    END AS 'MessageType',
 
-    message.text_data AS 'message.text_data',
+    message.text_data AS 'MessageTextData',
+
+    CASE message.status
+        WHEN 0 THEN 'ReceivedMessage/ReadByLocalUser'
+        WHEN 6 THEN 'SentUnsuccessful(NoResponseFromServer)'
+        WHEN 13 THEN 'SentSuccessful'
+    END AS 'MessageStatus',
 
     CASE
-        WHEN message_media.file_path IS NULL THEN '[N/A]'
+        WHEN message_media.file_path IS NULL THEN NULL
         ELSE message_media.file_path
-    END AS 'message_media.file_path',
+    END AS 'MessageMediaFilePath',
 
-    printf("%,d", message_media.file_size) AS 'message_media.file_size(bytes)',
+    message_media.message_url AS 'MessageMediaURL',
+
+    printf("%,d", message_media.file_size) AS 'MessageMediaFileSize(bytes)',
 
     CASE
-        WHEN message_media.width IS NULL THEN '[N/A]'
+        WHEN message_media.mime_type IS NULL THEN NULL
+        ELSE message_media.mime_type
+    END AS 'MessageMediaMimeType',
+
+    CASE
+        WHEN datetime(message_media.media_key_timestamp / 1000, 'UNIXEPOCH') = '1970-01-01 00:00:00' THEN NULL
+        ELSE datetime(message_media.media_key_timestamp / 1000, 'UNIXEPOCH')
+    END AS 'MessageMediaTimestamp(UTC)',
+
+    CASE
+        WHEN message_media.width IS NULL THEN NULL
         ELSE message_media.width
-    END AS 'message_media.width',
+    END AS 'MessageMediaWidth',
 
     CASE
-        WHEN message_media.height IS NULL THEN '[N/A]'
+        WHEN message_media.height IS NULL THEN NULL
         ELSE message_media.height
-    END AS 'message_media.height',
+    END AS 'MessageMediaHeight',
 
     CASE
-        WHEN message_location.latitude IS NULL THEN '[N/A]'
+        WHEN message_media.media_duration IS NULL THEN NULL
+        ELSE message_media.media_duration
+    END AS 'MessageMediaDuration',
+
+    CASE
+        WHEN message_location.latitude IS NULL THEN NULL
         ELSE message_location.latitude
-    END AS 'message_location.latitude',
+    END AS 'MessageLocationLatitude',
 
     CASE
-        WHEN message_location.longitude IS NULL THEN '[N/A]'
+        WHEN message_location.longitude IS NULL THEN NULL
         ELSE message_location.longitude
-    END AS 'message_location.longitude',
+    END AS 'MessageLocationLongitude',
 
     CASE
-        WHEN message_location.live_location_share_duration IS NULL THEN '[N/A]'
+        WHEN message_location.live_location_share_duration IS NULL THEN NULL
         ELSE message_location.live_location_share_duration
-    END AS 'message_location.live_location_share_duration',
+    END AS 'MessageLocationLiveLocationShareDuration(seconds)',
 
     CASE
-        WHEN message_location.live_location_final_latitude IS NULL THEN '[N/A]'
+        WHEN message_location.live_location_final_latitude IS NULL THEN NULL
         ELSE message_location.live_location_final_latitude
-    END AS 'message_location.live_location_final_latitude',
+    END AS 'FinalLiveLatitude',
 
     CASE
-        WHEN message_location.live_location_final_longitude IS NULL THEN '[N/A]'
+        WHEN message_location.live_location_final_longitude IS NULL THEN NULL
         ELSE message_location.live_location_final_longitude
-    END AS 'message_location.live_location_final_longitude',
+    END AS 'FinalLiveLongitude',
 
     CASE
-        WHEN message_location.live_location_final_timestamp IS NULL THEN '[N/A]'
+        WHEN message_location.live_location_final_timestamp IS NULL THEN NULL
         ELSE datetime(message_location.live_location_final_timestamp / 1000, 'UNIXEPOCH')
-    END AS 'message_location.live_location_final_timestamp',
+    END AS 'FinalLocationTimestamp',
 
     /* Source for each line of data */
-    'File: \data\data\com.whatsapp\databases\msgstore.db; Table: message(ROWID: ' || message._id || ')' AS 'DATA_SOURCE'
+    'File: /data/data/com.whatsapp/databases/msgstore.db; Table: message(ROWID:' || message._id || ')' AS 'DataSource'
 
 
 FROM message
     JOIN chat ON chat._id = message.chat_row_id
-    JOIN jid ON jid._id = chat.jid_row_id
+    LEFT JOIN jid ON jid._id = message.sender_jid_row_id
+    LEFT JOIN receipt_device ON receipt_device.message_row_id = message._id
+    LEFT JOIN receipt_user ON receipt_user.message_row_id = message._id
     LEFT JOIN message_media ON message_media.message_row_id = message._id
     LEFT JOIN message_location ON message_location.message_row_id = message._id
-    JOIN wa_contacts ON wa_contacts.jid = jid.raw_string
+    LEFT JOIN wa_contacts ON wa_contacts.jid = jid.raw_string
 
 
-ORDER BY message.timestamp ASC
+ORDER BY message.timestamp ASC;
+
+
+DETACH DATABASE whatsapp_db;
